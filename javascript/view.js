2f6e899201c246d146141295e4370c8a2dc1db92
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-01-17 21:30:10 +0200

    My commit message

--- javascript/view.js
+++ javascript/view.js
@@ -4,13 +4,4 @@ let apiMyprojectsApi = new TempApi.MyprojectsApi();import TempApi from '../src/i
     event.preventDefault();
     { location.href= '/create';}};window.onload = () => {let myprojectsId = window.location.pathname.replace('/view/','');apiMyprojectsApi.getmyprojects( myprojectsId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = pimage]').src = response.body.query.pimage ;document.querySelector('[annotationname = ptitle]').textContent = response.body.query.ptitle ;document.querySelector('[annotationname = pstart]').textContent = response.body.query.pstart ;document.querySelector('[annotationname = pend]').textContent = response.body.query.pend ;document.querySelector('[annotationname = pduration]').textContent = response.body.query.pduration ;document.querySelector('[annotationname = pabstract]').textContent = response.body.query.pabstract ;document.querySelector('[annotationname = pdescription]').textContent = response.body.query.pdescription ;document.querySelector('[annotationname = pga]').textContent = response.body.query.pga ;}});};document.getElementById('ikatkj').onclick = (event) => {
     event.preventDefault();
-    let myprojectsId = window.location.pathname.replace('/view/','');apiMyprojectsApi.deletemyprojects( myprojectsId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{ location.href= '/home';}}});};window.onload = () => {apiMyprojectsApi.getAllmyprojects((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements = document.getElementById("iiifs").querySelectorAll( "[dataitem='true']" );
-  data.forEach((item,i) => {
-    if(subDataElements.length > i)
-      {
-        console.log('There are no inside data elements');
-        subDataElements[i].addEventListener('click',() => {{ location.href= '/view/'+data[data.length -i -1]._id+'';}} )
-      }
-    });
-    
-    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
+    let myprojectsId = window.location.pathname.replace('/view/','');apiMyprojectsApi.deletemyprojects( myprojectsId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{ location.href= '/home';}}});};
