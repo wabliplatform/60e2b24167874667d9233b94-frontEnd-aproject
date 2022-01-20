09987e90ef1b984ecf5d3dee05c2c38b4b371d00
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-01-20 13:44:21 +0200

    My commit message

--- javascript/create.js
+++ javascript/create.js
@@ -1,5 +1,5 @@
-let apiMyprojectsApi = new TempApi.MyprojectsApi();import TempApi from '../src/index';let myprojects = new TempApi.Myprojects();document.getElementById('iq3ap').onclick = (event) => {
+document.getElementById('iq3ap').onclick = (event) => {
     event.preventDefault();
     { location.href= '/home';}};document.getElementById('icvac').onclick = (event) => {
     event.preventDefault();
-    myprojects['pimage'] = document.querySelector("[annotationname = 'pimage']").value;myprojects['ptitle'] = document.querySelector("[annotationname = 'ptitle']").value;myprojects['pstart'] = document.querySelector("[annotationname = 'pstart']").value;myprojects['pend'] = document.querySelector("[annotationname = 'pend']").value;myprojects['pduration'] = document.querySelector("[annotationname = 'pduration']").value;myprojects['pabstract'] = document.querySelector("[annotationname = 'pabstract']").value;myprojects['pdescription'] = document.querySelector("[annotationname = 'pdescription']").value;myprojects['pga'] = document.querySelector("[annotationname = 'pga']").value;myprojects['pwebsite'] = document.querySelector("[annotationname = 'pwebsite']").value;apiMyprojectsApi.createmyprojects( myprojects, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); { location.href= '/home/'+response.body.query._id+'';}}});};
+    { location.href= '/home';}};
