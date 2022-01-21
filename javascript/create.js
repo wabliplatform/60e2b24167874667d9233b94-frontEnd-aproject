9b5ee74f54775db69ae70d062be7c52ba440bf24
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-01-21 16:59:37 +0200

    My commit message

--- javascript/create.js
+++ javascript/create.js
@@ -1,5 +1,27 @@
 let apiMyprojectsApi = new TempApi.MyprojectsApi();import TempApi from '../src/index';let myprojects = new TempApi.Myprojects();document.getElementById('iq3ap').onclick = (event) => {
     event.preventDefault();
-    { location.href= '/home';}};document.getElementById('icvac').onclick = (event) => {
+    { location.href= '/home';}};
+ const uploadImage = async (event) => {
+        const file = event.target.files[0];
+        const base64 = await convertBase64(file);
+        return base64;
+      };const convertBase64 = (file) => {
+          return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+      
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+      
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+          });
+        };
+document.getElementById('formFile').addEventListener("change", async(e) => {
+            let imageBase64 = await uploadImage(e);
+        document.getElementById('formFile').setAttribute('data-image-base64' ,imageBase64);});
+document.getElementById('icvac').onclick = (event) => {
     event.preventDefault();
-    myprojects['pimage'] = document.querySelector("[annotationname = 'pimage']").value;myprojects['ptitle'] = document.querySelector("[annotationname = 'ptitle']").value;myprojects['pstart'] = document.querySelector("[annotationname = 'pstart']").value;myprojects['pend'] = document.querySelector("[annotationname = 'pend']").value;myprojects['pduration'] = document.querySelector("[annotationname = 'pduration']").value;myprojects['pabstract'] = document.querySelector("[annotationname = 'pabstract']").value;myprojects['pdescription'] = document.querySelector("[annotationname = 'pdescription']").value;myprojects['pga'] = document.querySelector("[annotationname = 'pga']").value;myprojects['pwebsite'] = document.querySelector("[annotationname = 'pwebsite']").value;apiMyprojectsApi.createmyprojects( myprojects, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); { location.href= '/home/'+response.body.query._id+'';}}});};
+    myprojects['pimage'] = document.querySelector("[annotationname = 'pimage']").getAttribute('data-image-base64');myprojects['ptitle'] = document.querySelector("[annotationname = 'ptitle']").value;myprojects['pstart'] = document.querySelector("[annotationname = 'pstart']").value;myprojects['pend'] = document.querySelector("[annotationname = 'pend']").value;myprojects['pduration'] = document.querySelector("[annotationname = 'pduration']").value;myprojects['pabstract'] = document.querySelector("[annotationname = 'pabstract']").value;myprojects['pdescription'] = document.querySelector("[annotationname = 'pdescription']").value;myprojects['pga'] = document.querySelector("[annotationname = 'pga']").value;myprojects['pwebsite'] = document.querySelector("[annotationname = 'pwebsite']").value;apiMyprojectsApi.createmyprojects( myprojects, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); { location.href= '/home/'+response.body.query._id+'';}}});};
--- javascript/home.js
+++ javascript/home.js
@@ -33,4 +33,70 @@ let apiMyprojectsApi = new TempApi.MyprojectsApi();import TempApi from '../src/i
       }
     });
     
+    let numberOfPages = Math.ceil(data.length/subDataElements.length);
+    let pagination = document.querySelector('[paginationlist="true"]');
+    let chunk = 1;
+
+    for(let i =1; i<pagination.children.length-1;i){
+      pagination.removeChild(pagination.children[i]);
+    }
+
+    for (let i = 0; i < numberOfPages; i++) {
+      let child = document.createElement("li");
+      child.classList.add("page-item");
+      if (i === numberOfPages - 1) {
+        child.classList.add("active");
+      }
+      let insideChild = document.createElement("a");
+      insideChild.classList.add("page-link");
+      let textnode = document.createTextNode(numberOfPages - i);
+      insideChild.appendChild(textnode);
+      insideChild.setAttribute("href", "#!");
+      child.appendChild(insideChild);
+      child.onclick = function () {
+        if(chunk !== numberOfPages-i){
+          chunk = numberOfPages-i;
+          onClickPaginationButton(chunk, child);
+        }
+      };
+      pagination.insertBefore(child, pagination.children[1]);
+    }
+    
+    if ( pagination.children[0].getAttribute("paginationnewer") === "true" && numberOfPages > 0 ) {
+      pagination.children[0].onclick = function () {
+        if(chunk !== 1){
+          chunk = 1;
+          onClickPaginationButton(1, pagination.children[0]);
+        }
+      };
+    }
+
+    if ( pagination.children[numberOfPages + 1].getAttribute( "paginationolder" ) === "true" && numberOfPages > 0 ) {
+      pagination.children[numberOfPages + 1].onclick = function () {
+        if(chunk !== numberOfPages){
+          chunk = numberOfPages;
+          onClickPaginationButton( numberOfPages, pagination.children[numberOfPages + 1]);
+        } 
+      };
+    }
+
+    if ( pagination.children[0].getAttribute("paginationprevious") === "true" && numberOfPages > 0 ) {
+      pagination.children[0].onclick = function () {
+        if(chunk !== returnChunkIndex(chunk,numberOfPages,'-')){
+          chunk = returnChunkIndex(chunk,numberOfPages,'-');
+          onClickPaginationButton(chunk, pagination.children[0]);
+        }
+      };
+    }
+
+    if ( pagination.children[numberOfPages + 1].getAttribute("paginationnext") === "true" && numberOfPages > 0) {
+      pagination.children[numberOfPages + 1].onclick = function () {
+        if(chunk !== returnChunkIndex(chunk,numberOfPages,'+')){
+          chunk = returnChunkIndex(chunk,numberOfPages,'+');
+          onClickPaginationButton(chunk, pagination.children[numberOfPages+1]);
+        }
+      };
+    }
+
+  
     [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
