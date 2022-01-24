53511220ca63386b2b0a5e28cdcc103e4b719156
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-01-24 17:50:01 +0200

    My commit message

--- javascript/home.js
+++ javascript/home.js
@@ -34,69 +34,93 @@ let apiMyprojectsApi = new TempApi.MyprojectsApi();import TempApi from '../src/i
     });
     
     let numberOfPages = Math.ceil(data.length/subDataElements.length);
-    let pagination = document.querySelector('[paginationlist="true"]');
+    let pagination = document.querySelector('[pagination-list="true"]');
     let chunk = 1;
 
-    for(let i =1; i<pagination.children.length-1;i){
-      pagination.removeChild(pagination.children[i]);
-    }
+      let paginationAttributes = [
+        "pagination-first",
+        "pagination-last",
+        "pagination-previous",
+        "pagination-next",
+      ];
 
-    for (let i = 0; i < numberOfPages; i++) {
-      let child = document.createElement("li");
-      child.classList.add("page-item");
-      if (i === numberOfPages - 1) {
-        child.classList.add("active");
+      for (let i = 0; i < pagination.children.length; ) {
+        let foundAttr = false;
+
+        paginationAttributes.forEach((attr) => {
+          if (pagination.children[i].attributes.getNamedItem(attr) !== null) {
+            foundAttr = true;
+          }
+        });
+        if (foundAttr === false) {
+          pagination.removeChild(pagination.children[i]);
+        } else {
+          i++;
+        }
       }
-      let insideChild = document.createElement("a");
-      insideChild.classList.add("page-link");
-      let textnode = document.createTextNode(numberOfPages - i);
-      insideChild.appendChild(textnode);
-      insideChild.setAttribute("href", "#!");
-      child.appendChild(insideChild);
-      child.onclick = function () {
-        if(chunk !== numberOfPages-i){
-          chunk = numberOfPages-i;
-          onClickPaginationButton(chunk, child);
-        }
-      };
-      pagination.insertBefore(child, pagination.children[1]);
-    }
-    
-    if ( pagination.children[0].getAttribute("paginationnewer") === "true" && numberOfPages > 0 ) {
-      pagination.children[0].onclick = function () {
-        if(chunk !== 1){
-          chunk = 1;
-          onClickPaginationButton(1, pagination.children[0]);
-        }
-      };
-    }
 
-    if ( pagination.children[numberOfPages + 1].getAttribute( "paginationolder" ) === "true" && numberOfPages > 0 ) {
-      pagination.children[numberOfPages + 1].onclick = function () {
-        if(chunk !== numberOfPages){
-          chunk = numberOfPages;
-          onClickPaginationButton( numberOfPages, pagination.children[numberOfPages + 1]);
-        } 
-      };
-    }
+      for (let i = 0; i < numberOfPages; i++) {
+        let child = document.createElement("li");
+        child.classList.add("page-item");
+        if (i === numberOfPages - 1) {
+          child.classList.add("active");
+        }
+        let insideChild = document.createElement("a");
+        insideChild.classList.add("page-link");
+        let textnode = document.createTextNode(numberOfPages - i);
+        insideChild.appendChild(textnode);
+        insideChild.setAttribute("href", "#!");
+        child.appendChild(insideChild);
+        child.onclick = function () {
+          if (chunk !== numberOfPages - i) {
+            chunk = numberOfPages - i;
+            onClickPaginationButton(chunk, pagination);
+          }
+        };
 
-    if ( pagination.children[0].getAttribute("paginationprevious") === "true" && numberOfPages > 0 ) {
-      pagination.children[0].onclick = function () {
-        if(chunk !== returnChunkIndex(chunk,numberOfPages,'-')){
-          chunk = returnChunkIndex(chunk,numberOfPages,'-');
-          onClickPaginationButton(chunk, pagination.children[0]);
+        let numberOfFrontButtons =  findTypeOfPagination(pagination); 
+        pagination.insertBefore(child, pagination.children[numberOfFrontButtons]);
+
+      }
+
+      for (let i = 0; i < pagination.children.length; i++) {
+        let child = pagination.children[i];
+        if ( child.getAttribute("pagination-first") === "true" && numberOfPages > 0 ) {
+          child.onclick = function () {
+            if(chunk !== 1){
+              chunk = 1;
+              onClickPaginationButton(1, pagination);
+            }
+          };
+        }
+
+        if ( child.getAttribute( "pagination-last" ) === "true" && numberOfPages > 0 ) {
+          child.onclick = function () {
+            if(chunk !== numberOfPages){
+              chunk = numberOfPages;
+              onClickPaginationButton( numberOfPages, pagination);
+            }
+          };
+        }
+
+        if ( child.getAttribute("pagination-previous") === "true" && numberOfPages > 0 ) {
+          child.onclick = function () {
+            if(chunk !== returnChunkIndex(chunk,numberOfPages,'-')){
+              chunk = returnChunkIndex(chunk,numberOfPages,'-');
+              onClickPaginationButton(chunk, pagination);
+            }
+          };
+        }
+
+        if ( child.getAttribute("pagination-next") === "true" && numberOfPages > 0) {
+          child.onclick = function () {
+            if(chunk !== returnChunkIndex(chunk,numberOfPages,'+')){
+              chunk = returnChunkIndex(chunk,numberOfPages,'+');
+              onClickPaginationButton(chunk, pagination);
+            }
+          };
         }
       };
-    }
-
-    if ( pagination.children[numberOfPages + 1].getAttribute("paginationnext") === "true" && numberOfPages > 0) {
-      pagination.children[numberOfPages + 1].onclick = function () {
-        if(chunk !== returnChunkIndex(chunk,numberOfPages,'+')){
-          chunk = returnChunkIndex(chunk,numberOfPages,'+');
-          onClickPaginationButton(chunk, pagination.children[numberOfPages+1]);
-        }
-      };
-    }
 
   
     [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
