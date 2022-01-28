e6adff40542f9c27a3b47b7febadb3354c74b1a9
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-01-28 20:18:52 +0200

    My commit message

--- javascript/viewtask.js
+++ javascript/viewtask.js
@@ -4,7 +4,7 @@ let apiTasksApi = new TempApi.TasksApi();import TempApi from '../src/index';docu
     event.preventDefault();
     { window.document.location = '#!';}};document.getElementById('ihn45').onclick = (event) => {
     event.preventDefault();
-    { window.document.location = '#!';}};window.onload = () => {let tasksId = window.location.pathname.replace('/viewtask/','');apiTasksApi.gettasks( tasksId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = tname]').textContent = response.body.query.tname ;document.querySelector('[annotationname = sdate]').textContent = response.body.query.sdate ;document.querySelector('[annotationname = edate]').textContent = response.body.query.edate ;document.querySelector('[annotationname = vpms]').textContent = response.body.query.vpms ;document.querySelector('[annotationname = tdescription]').textContent = response.body.query.tdescription ;}});};window.onload = () => {apiTasksApi.getAlltasks((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements = document.getElementById("i3yos").querySelectorAll( "[dataitem='true']" );
+    { window.document.location = '#!';}};window.onload = () => {let tasksId = window.location.pathname.replace('/viewtask/','');apiTasksApi.gettasks( tasksId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = tname]').textContent = response.body.query.tname ;document.querySelector('[annotationname = sdate]').textContent = response.body.query.sdate ;document.querySelector('[annotationname = edate]').textContent = response.body.query.edate ;document.querySelector('[annotationname = vpms]').textContent = response.body.query.vpms ;document.querySelector('[annotationname = tdescription]').textContent = response.body.query.tdescription ;}});};window.onload = () => {apiTasksApi.getAlltasks((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements = document.getElementById("iswei").querySelectorAll( "[dataitem='true']" );
   data.forEach((item,i) => {
     if(subDataElements.length > i)
       {
@@ -13,16 +13,13 @@ let apiTasksApi = new TempApi.TasksApi();import TempApi from '../src/index';docu
       }
     });
     
-    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};document.getElementById('it07k').onclick = (event) => {
-    event.preventDefault();
-    { window.document.location = '#!';}};document.getElementById('iqwvz').onclick = (event) => {
-    event.preventDefault();
-    { window.document.location = '#!';}};document.getElementById('iwy1k').onclick = (event) => {
-    event.preventDefault();
-    { window.document.location = '#!';}};document.getElementById('i4tgg').onclick = (event) => {
-    event.preventDefault();
-    { window.document.location = '#!';}};document.getElementById('iuzsj').onclick = (event) => {
-    event.preventDefault();
-    { window.document.location = '#!';}};document.getElementById('iniff').onclick = (event) => {
-    event.preventDefault();
-    { window.document.location = '#!';}};
+    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};window.onload = () => {apiTasksApi.getAlltasks((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements = document.getElementById("i3yos").querySelectorAll( "[dataitem='true']" );
+  data.forEach((item,i) => {
+    if(subDataElements.length > i)
+      {
+        console.log('There are no inside data elements');
+        subDataElements[i].addEventListener('click',() => {{ location.href= '/viewtask/'+data[data.length -i -1]._id+'';}} )
+      }
+    });
+    
+    [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
