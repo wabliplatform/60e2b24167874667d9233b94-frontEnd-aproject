703fde324eab22a8e354860f2c1ac19b085932b4
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-01-28 20:35:05 +0200

    My commit message

--- javascript/viewtask.js
+++ javascript/viewtask.js
@@ -4,11 +4,19 @@ let apiTasksApi = new TempApi.TasksApi();import TempApi from '../src/index';docu
     event.preventDefault();
     { window.document.location = '#!';}};document.getElementById('ihn45').onclick = (event) => {
     event.preventDefault();
-    { window.document.location = '#!';}};window.onload = () => {let tasksId = window.location.pathname.replace('/viewtask/','');apiTasksApi.gettasks( tasksId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = tname]').textContent = response.body.query.tname ;document.querySelector('[annotationname = sdate]').textContent = response.body.query.sdate ;document.querySelector('[annotationname = edate]').textContent = response.body.query.edate ;document.querySelector('[annotationname = vpms]').textContent = response.body.query.vpms ;document.querySelector('[annotationname = tdescription]').textContent = response.body.query.tdescription ;}});};window.onload = () => {apiTasksApi.getAlltasks((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements = document.getElementById("iswei").querySelectorAll( "[dataitem='true']" );
+    { window.document.location = '#!';}};window.onload = () => {let tasksId = window.location.pathname.replace('/viewtask/','');apiTasksApi.gettasks( tasksId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = tname]').textContent = response.body.query.tname ;document.querySelector('[annotationname = sdate]').textContent = response.body.query.sdate ;document.querySelector('[annotationname = edate]').textContent = response.body.query.edate ;document.querySelector('[annotationname = vpms]').textContent = response.body.query.vpms ;document.querySelector('[annotationname = tdescription]').textContent = response.body.query.tdescription ;}});};window.onload = () => {apiTasksApi.getAlltasks((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements = document.getElementById("iihan").querySelectorAll( "[dataitem='true']" );
   data.forEach((item,i) => {
     if(subDataElements.length > i)
       {
-        console.log('There are no inside data elements');
+        try { 
+        const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'tname']");
+        if(insideSubDataElement !== null){
+          insideSubDataElement.textContent = data[data.length -i -1].tname;
+        }
+        else if(subDataElements[i].getAttribute('annotationname') === 'tname'){
+          subDataElements[i].textContent = data[data.length -i -1].tname;
+        }
+       } catch (e) { console.log(e) };
         subDataElements[i].addEventListener('click',() => {{ location.href= '/viewtask/'+data[data.length -i -1]._id+'';}} )
       }
     });
